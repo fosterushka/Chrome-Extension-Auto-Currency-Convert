c0fd9bc2ce4a7a89ba35dd6dfa370531afdc1fdc
AydievMansur <man1080@yandex.ru>
2019-11-17 20:14:15 +0400

    + git ignore -popup /currency-rate need pass var x & dom elements into plugin right from page

--- popup.js
+++
@@ -1,7 +0,0 @@
-let x = document.getElementsByClassName("price");
-for (i = 0; i < x.length; i++) {
-    var text = x.item(i).innerText.replace('$', '');
-    var ready = parseFloat(text, 10) * 64;
-    var steam = ready - parseInt((ready * .13));
-    x.item(i).innerText = (ready + '/' + steam)
-}
--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-window.addEventListener('load', function () {
-    let x = document.getElementsByClassName("price");
+// console.log('test1');
+// var test = '123';
+// var x = document.getElementsByClassName("price");
+// document.getElementById("start").addEventListener("click", function () {
+//     if (x === HTMLCollection) {
+//         // let x = document.getElementsByClassName("price");
+//         for (let i = 0; i < x.length; i++) {
+//             var text = x.item(i).innerText.replace('$', '');
+//             var ready = parseFloat(text, 10) * 63;
+//             var steam = ready - parseInt((ready * .09));
+//             x.item(i).innerText = parseInt(ready) + '/' + parseInt(steam)
+//         }
+//         alert('Успешно')
+//     } else {
+//         alert('Ошибка')
+//     }
+// });
+// console.log('test2');
+var x = document.getElementsByClassName("price");
 
-    document.getElementById("start").addEventListener("click", function(){
-        if (x === HTMLCollection) {
-            // let x = document.getElementsByClassName("price");
-            for (i = 0; i < x.length; i++) {
-                var text = x.item(i).innerText.replace('$', '');
-                var ready = parseFloat(text, 10) * 63;
-                var steam = ready - parseInt((ready * .09));
-                x.item(i).innerText = parseInt(ready) + '/' + parseInt(steam)
-            }
-            alert('Успешно')
-        } else {
-            alert('Ошибка')
-        }
-    });
-});
+for (let i = 0; i < x.length; i++) {
+    var text = x.item(i).innerText.replace('$', '');
+    var ready = parseFloat(text, 10) * 63.75;
+    var steam = ready - parseInt((ready * .13));
+    x.item(i).innerText = parseInt(ready) + '/' + parseInt(steam)
+}
