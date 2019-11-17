6498f26c7800d2283d9cc6e5bc96cb5f786750ed
AydievMansur <man1080@yandex.ru>
2019-11-17 21:11:36 +0400

    add background to allow work with page DOM

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({hide:true}), function () {
+        console.log('OKAY HIDE')
+    }
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined,function () {
+   chrome.declarativeContent.onPageChanged.addRules([{
+       conditions: [new chrome.declarativeContent.PageStateMatcher({
+           pageUrl: {hostEquals: 'www.swap.gg'},
+       })
+       ],
+        action: [new chrome.declarativeContent.ShowPageAction()]
+   }]);
+});
--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-// console.log('test1');
-// var test = '123';
-// var x = document.getElementsByClassName("price");
-// document.getElementById("start").addEventListener("click", function () {
-//     if (x === HTMLCollection) {
-//         // let x = document.getElementsByClassName("price");
-//         for (let i = 0; i < x.length; i++) {
-//             var text = x.item(i).innerText.replace('$', '');
-//             var ready = parseFloat(text, 10) * 63;
-//             var steam = ready - parseInt((ready * .09));
-//             x.item(i).innerText = parseInt(ready) + '/' + parseInt(steam)
-//         }
-//         alert('Успешно')
-//     } else {
-//         alert('Ошибка')
-//     }
-// });
-// console.log('test2');
 var x = document.getElementsByClassName("price");
+
+
 
 for (let i = 0; i < x.length; i++) {
     var text = x.item(i).innerText.replace('$', '');
