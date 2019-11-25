5acba2afd7a282879f0015511c2242a396a7fa76
AydievMansur <man1080@yandex.ru>
2019-11-26 00:26:43 +0400

    fix initialization and parse with c/d 5sec up: .ignore

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 $.getJSON('https://free.currconv.com/api/v7/convert?q=USD_RUB&compact=ultra&apiKey=17fd28c2f5a1d66c7522',function (data) {
     const currency = data.USD_RUB.toString().substring(0, 5);
-    console.log(parseFloat(currency));
 
-    const x = document.getElementsByClassName("price");
-
-    for (let i = 0; i < x.length; i++) {
-        var text = x.item(0).innerText.replace('$', '');
-        var ready = parseFloat(text, 10) * currency;
-        var steam = ready - parseInt((ready * .13));
-        x.item(i).innerText = parseInt(ready) + '/' + parseInt(steam)
-    }
-
+    setTimeout(function(){
+        let x = document.getElementsByClassName("price");
+        for (i = 0; i < x.length; i++) {
+            var text = x.item(i).innerText.replace('$', '');
+            var ready = parseFloat(text, 10) * 64;
+            var steam = ready - parseInt((ready * .10));
+            x.item(i).innerText = (ready + '/' + steam)
+        }
+    },5000)
 });
