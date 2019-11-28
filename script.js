5c7cace06d38fead2418a0ae8cf6ac47d6f77ad5
AydievMansur <man1080@yandex.ru>
2019-11-28 04:31:49 +0400

    /improve calculating +make test ui -remove open api : WIP

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-setInterval(startParse,1000);
-setTimeout(startParse, 1000, undefined);
+var currency = 63.99;
+
+setInterval(startParse,5000);
+setTimeout(startParse, 0);
 function startParse() {
-    $.getJSON('https://free.currconv.com/api/v7/convert?q=USD_RUB&compact=ultra&apiKey=17fd28c2f5a1d66c7522', function (data) {
-        const currency = data.USD_RUB.toString().substring(0, 5);
-        let x = document.querySelectorAll('.price:not(.checked-item)');
-        let y = document.querySelectorAll('.price .checked-item');
-        if (x) {
-            for (i = 0; i < x.length; i++) {
-                let text = x.item(i).innerText.replace('$', '');
-                let ready = parseInt(text, 10) * parseInt(currency);
-                let steam = ready - parseInt((ready * .10));
-                x.item(i).classList.add("checked-item");
-                x.item(i).innerText = ('₽' + ready + '/' + steam);
-            }
-        } else {
-            for (i = 0; i < y.length; i++) {
-                let text = y.item(i).innerText.replace('$', '');
-                let ready = parseInt(text, 10) * parseInt(currency);
-                let steam = ready - parseInt((ready * .10));
-                y.item(i).innerText = ('₽' + ready + '/' + steam);
-            }
+    let x = document.querySelectorAll('.price:not(.checked-item)');
+    let y = document.querySelectorAll('.price .checked-item');
+    if (x) {
+        for (i = 0; i < x.length; i++) {
+            let text = x.item(i).innerText.replace('$', '');
+            let ready = parseFloat(text) * currency;
+            x.item(i).classList.add("checked-item");
+            x.item(i).innerText = ('₽' + ready.toFixed(2));
         }
-    });
+    } else {
+        for (i = 0; i < y.length; i++) {
+            let text = y.item(i).innerText.replace('$', '');
+            let ready = parseFloat(text) * currency;
+            y.item(i).innerText = ('₽' + ready.toFixed(2));
+        }
+    }
 }
 
 // x.item(i).style.color = "#008000";
+// let steam = ready - parseInt((ready * .10));
 
