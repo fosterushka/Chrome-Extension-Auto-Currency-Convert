2d90cc8db558ecc3293fbf6b2e2ad324ccadb451
idevmans <man1080@yandex.ru>
2021-05-03 00:15:40 +0400

    remove background,rework script.js | the code doesn't work i broke it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,40 @@
- console.log('ayman exchange injected');
- async function getExchangeRate() {
-    if (localStorage.hasOwnProperty('currency')) {
-        let getApiCurrency = await fetch('https://api.exchangeratesapi.io/latest?symbols=USD,RUB');
-        console.log(JSON.stringify(getApiCurrency));
+console.log('ayman exchange injected');
+
+//if (!localStorage.hasOwnProperty('currency')) {
+//    let getApiCurrency = fetch('https://api.exchangeratesapi.io/latest?symbols=USD,RUB');
+//    console.log(getApiCurrency);
+//    console.log(JSON.stringify(getApiCurrency));
+//    let rate_currency = window.localStorage.setItem('rate_currency', (getApiCurrency));
+//    console.log(JSON.stringify(getApiCurrency));
+//}
+//setInterval(startParse, 1000);
+//setTimeout(startParse, 0);
+
+const websitesUtils = [
+    {"website":'https://swap.gg/*', "class" : "div.item span.p"},
+    {"website":'https://amazon.com/*', "class" : ".price"},
+    {"website":'https://dmarket.com/*', "class" : ".price"},
+];
+console.log(websitesUtils);
+websitesUtils.find(window.location.href);
+console.log(websitesUtils);
+
+
+function startParse() {
+    let x = document.querySelectorAll('div.item span.p:not(.checked-item)');
+    let y = document.querySelectorAll('div.item span.p.checked-item');
+    if (x) {
+        for (i = 0; i < x.length; i++) {
+            let text = x.item(i).innerText.replace('$', '');
+            let ready = parseFloat(text) * rub;
+            x.item(i).classList.add("checked-item");
+            x.item(i).innerText = ('₽' + ready.toFixed(2));
+        }
+    } else {
+        for (i = 0; i < y.length; i++) {
+            let text = y.item(i).innerText.replace('$', '');
+            let ready = parseFloat(text) * rub;
+            y.item(i).innerText = ('₽' + ready.toFixed(2));
+        }
     }
-    let rate_currency = window.localStorage.setItem('rate_currency', (getApiCurrency));
-   
-    await (rate_currency);
-    let rub = rate_currency.responseJSON.rates.RUB; // todo
-    setInterval(startParse, 1000);
-    setTimeout(startParse, 0);
-   
-    const websitesUtils = new Map([
-        ['https://swap.gg/*', 'div.item span.p'],
-        ['https://amazon.com/*', '.price'], //test class
-        ['https://dmarket.com/*', '.price'], //test class
-    ]);
-    websitesUtils.find(window.location.href);
-    console.log(websitesUtils);
 }
- 
-
- function startParse() {
-     console.log('in func');
-     let x = document.querySelectorAll('div.item span.p:not(.checked-item)');
-     let y = document.querySelectorAll('div.item span.p.checked-item');
-     if (x) {
-         for (i = 0; i < x.length; i++) {
-             let text = x.item(i).innerText.replace('$', '');
-             let ready = parseFloat(text) * rub;
-             x.item(i).classList.add("checked-item");
-             x.item(i).innerText = ('₽' + ready.toFixed(2));
-         }
-     } else {
-         for (i = 0; i < y.length; i++) {
-             let text = y.item(i).innerText.replace('$', '');
-             let ready = parseFloat(text) * rub;
-             y.item(i).innerText = ('₽' + ready.toFixed(2));
-         }
-     }
- }
-
-chrome.scripting.executeScript({
-    function: startParse,getExchangeRate
-});
